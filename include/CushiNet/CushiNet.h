abc7b2ae587bed9cfeba39c0c9ece69925211f28
RivelBop <rivelbop@gmail.com>
2026-07-13 04:26:36 -0400

    Added `CushiNet::RunCallbacks()`

--- include/CushiNet/CushiNet.h
+++ include/CushiNet/CushiNet.h
@@ -4,6 +4,15 @@ namespace CushiNet
 {
 
 /**
+ * Runs the callbacks from Valve's `SteamNetworkingSockets()`.
+ *
+ * Use this if you prefer to run all callbacks from one place
+ * BEFORE calling the `Update(false)` methods of your `Server`
+ * and/or `Client`.
+ */
+void RunCallbacks();
+
+/**
  * Close all connections and listen sockets, and free all resources
  * from Valve's GameNetworkingSockets.
  *
--- src/CushiNet.cpp
+++ src/CushiNet.cpp
@@ -1,6 +1,14 @@
 #include <CushiNet/CushiNet.h>
 #include <steam/steamnetworkingsockets.h>
 
+void CushiNet::RunCallbacks()
+{
+    ISteamNetworkingSockets *networkInterface{ SteamNetworkingSockets() };
+    if (networkInterface) {
+        networkInterface->RunCallbacks();
+    }
+}
+
 void CushiNet::Dispose()
 {
     GameNetworkingSockets_Kill();
