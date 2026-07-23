2e2781565b79105fa84cf86164252db8ee7b0d5b
RivelBop <rivelbop@gmail.com>
2026-07-22 21:24:09 -0400

    Added IPv4 Address Parsing

--- include/CushiNet/CushiNet.h
+++ include/CushiNet/CushiNet.h
@@ -1,7 +1,31 @@
 #pragma once
 
+#include <string>
+
+using uint8 = unsigned char;
+using uint32 = unsigned int;
+
 namespace CushiNet
 {
+
+/**
+ * Pass a `std::string` in IPv4 format:
+ * `[0-255].[0-255].[0-255].[0-255]`
+ *
+ * Returns a value representing your IPv4 address that you can pass into:
+ * `SteamNetworkingIPAddr::SetIPv4()`.
+ *
+ * Throws `std::invalid_argument` if the IPv4 address is invalidly formatted.
+ */
+uint32 ParseIPv4Address(const std::string &ip);
+
+/**
+ * Pass 4 octets representing each IPv4 address segment.
+ *
+ * Returns a value representing your IPv4 address that you can pass into:
+ * `SteamNetworkingIPAddr::SetIPv4()`.
+ */
+uint32 ParseIPv4Address(uint8 first, uint8 second, uint8 third, uint8 fourth);
 
 /**
  * Runs the callbacks from Valve's `SteamNetworkingSockets()`.
--- src/CushiNet.cpp
+++ src/CushiNet.cpp
@@ -1,5 +1,35 @@
+#include <stdexcept>
+
 #include <CushiNet/CushiNet.h>
 #include <steam/steamnetworkingsockets.h>
+
+uint32 CushiNet::ParseIPv4Address(const std::string &ip)
+{
+    size_t firstDot{ ip.find_first_of('.') };
+    if (firstDot == std::string::npos) {
+        throw std::invalid_argument("Invalid IPv4 Address Format.");
+    }
+
+    size_t secondDot{ ip.find_first_of('.', firstDot + 1) };
+    if (secondDot == std::string::npos) {
+        throw std::invalid_argument("Invalid IPv4 Address Format.");
+    }
+
+    size_t thirdDot{ ip.find_first_of('.', secondDot + 1) };
+    if (thirdDot == std::string::npos) {
+        throw std::invalid_argument("Invalid IPv4 Address Format.");
+    }
+
+    return ((uint32)std::stoi(ip.substr(0, firstDot)) << 24) |
+           ((uint32)std::stoi(ip.substr(firstDot + 1, secondDot - (firstDot + 1))) << 16) |
+           ((uint32)std::stoi(ip.substr(secondDot + 1, thirdDot - (secondDot + 1))) << 8) |
+           ((uint32)std::stoi(ip.substr(thirdDot + 1)));
+}
+
+uint32 CushiNet::ParseIPv4Address(uint8 first, uint8 second, uint8 third, uint8 fourth)
+{
+    return ((uint32)first << 24) | ((uint32)second << 16) | ((uint32)third << 8) | (uint32)fourth;
+}
 
 void CushiNet::RunCallbacks()
 {
